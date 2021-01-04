GalAster/rune

Start work on next compiler version v2

crates/rune/src/compiling/v2/assemble/item_fn.rs
@@ -1,0 +1,51 @@
+use crate::compiling::v2::assemble::prelude::*;
+
+impl AssembleFn for ast::ItemFn {
+    fn assemble_fn(
+        &self,
+        c: &mut Compiler<'_>,
+        instance_fn: bool,
+    ) -> CompileResult<rune_im::Block> {
+        let span = self.span();
+        log::trace!("ItemFn => {:?}", c.source.source(span));
+
+        let block = c.sm.block();
+
+        let mut first = true;
+
+        for (arg, _) in &self.args {
+            let span = arg.span();
+            let value = block.input();
+            let first = std::mem::take(&mut first);
+
+            match arg {
+                ast::FnArg::SelfValue(s) => {
+                    let span = s.span();
+
+                    if !instance_fn || !first {
+                        return Err(CompileError::new(span, CompileErrorKind::UnsupportedSelf));
+                    }
+
+                    c.scope.declare(span, "self", value)?;
+                    continue;
+                }
+                ast::FnArg::Pat(ast::Pat::PatPath(path)) => {
+                    if let Some(ident) = path.path.try_as_ident() {
+                        let name = c.resolve(ident)?;
+                        c.scope.declare(span, &name, value)?;
+                        continue;
+                    } else {
+                        return Err(CompileError::msg(span, "path not supported yet"));
+                    }
+                }
+                _ => {
+                    return Err(CompileError::msg(span, "argument not supported yet"));
+                }
+            }
+        }
+
+        let value = self.body.assemble(&block, c)?;
+        block.return_(value);
+        Ok(block)
+    }
+}

crates/rune/src/compiling/v2/assemble/mod.rs
@@ -1,0 +1,22 @@
+use crate::compiling::v2::Compiler;
+use crate::compiling::CompileResult;
+use rune_im::{Block, ValueId};
+
+mod block;
+mod expr;
+mod item_fn;
+mod prelude;
+
+/// Compiler trait implemented for things that can be compiled.
+///
+/// This is the new compiler trait to implement.
+pub(crate) trait Assemble {
+    /// Walk the current type with the given item.
+    fn assemble(&self, block: &Block, c: &mut Compiler<'_>) -> CompileResult<ValueId>;
+}
+
+/// Assemble a function.
+pub(crate) trait AssembleFn {
+    /// Assemble a function.
+    fn assemble_fn(&self, c: &mut Compiler<'_>, instance_fn: bool) -> CompileResult<Block>;
+}

crates/rune/src/compiling/v2/assemble/prelude.rs
@@ -1,0 +1,5 @@
+pub(crate) use crate::ast;
+pub(crate) use crate::compiling::v2::{Assemble, AssembleFn, Compiler};
+pub(crate) use crate::parsing::ParseErrorKind;
+pub(crate) use crate::{CompileError, CompileErrorKind, CompileResult, Spanned};
+pub(crate) use rune_im::{Block, Constant, ValueId};

crates/rune/src/compiling/v2/mod.rs
@@ -1,0 +1,52 @@
+use crate::compiling::CompileVisitor;
+use crate::parsing::{Resolve, ResolveError};
+use crate::query::Query;
+use crate::shared::Consts;
+use crate::{Options, Storage, UnitBuilder, Warnings};
+use runestick::{Context, Location, Source, Span};
+use std::sync::Arc;
+
+pub(crate) mod assemble;
+pub(crate) mod scope;
+
+pub(crate) use self::assemble::{Assemble, AssembleFn};
+
+#[allow(unused)]
+pub(crate) struct Compiler<'a> {
+    /// Program being compiled.
+    pub(crate) sm: &'a mut rune_im::Program,
+    /// The source id of the source.
+    pub(crate) location: Location,
+    /// The source we are compiling for.
+    pub(crate) source: &'a Arc<Source>,
+    /// The current scope stack.
+    pub(crate) scope: scope::Stack,
+    /// The current macro context.
+    pub(crate) storage: &'a Storage,
+    /// The context we are compiling for.
+    pub(crate) context: &'a Context,
+    /// Constants storage.
+    pub(crate) consts: &'a Consts,
+    /// Query system to compile required items.
+    pub(crate) query: &'a Query,
+    /// The compilation unit we are compiling for.
+    pub(crate) unit: UnitBuilder,
+    /// Context for which to emit warnings.
+    pub(crate) contexts: Vec<Span>,
+    /// Enabled optimizations.
+    pub(crate) options: &'a Options,
+    /// Compilation warnings.
+    pub(crate) warnings: &'a mut Warnings,
+    /// Compiler visitor.
+    pub(crate) visitor: &'a mut dyn CompileVisitor,
+}
+
+impl<'a> Compiler<'a> {
+    /// Resolve the given value.
+    pub(crate) fn resolve<T>(&self, value: &T) -> Result<T::Output, ResolveError>
+    where
+        T: Resolve<'a>,
+    {
+        value.resolve(&*self.storage, self.source)
+    }
+}

udoprog/runestick

crates/rune/src/compiling/v2/assemble/item_fn.rs
@@ -1,0 +1,51 @@
+use crate::compiling::v2::assemble::prelude::*;
+
+impl AssembleFn for ast::ItemFn {
+    fn assemble_fn(
+        &self,
+        c: &mut Compiler<'_>,
+        instance_fn: bool,
+    ) -> CompileResult<rune_ssa::Block> {
+        let span = self.span();
+        log::trace!("ItemFn => {:?}", c.source.source(span));
+
+        let block = c.sm.block();
+
+        let mut first = true;
+
+        for (arg, _) in &self.args {
+            let span = arg.span();
+            let value = block.input();
+            let first = std::mem::take(&mut first);
+
+            match arg {
+                ast::FnArg::SelfValue(s) => {
+                    let span = s.span();
+
+                    if !instance_fn || !first {
+                        return Err(CompileError::new(span, CompileErrorKind::UnsupportedSelf));
+                    }
+
+                    c.scope.declare(span, "self", value)?;
+                    continue;
+                }
+                ast::FnArg::Pat(ast::Pat::PatPath(path)) => {
+                    if let Some(ident) = path.path.try_as_ident() {
+                        let name = c.resolve(ident)?;
+                        c.scope.declare(span, &name, value)?;
+                        continue;
+                    } else {
+                        return Err(CompileError::msg(span, "path not supported yet"));
+                    }
+                }
+                _ => {
+                    return Err(CompileError::msg(span, "argument not supported yet"));
+                }
+            }
+        }
+
+        let value = self.body.assemble(&block, c)?;
+        block.return_(value);
+        Ok(block)
+    }
+}

crates/rune/src/compiling/v2/assemble/mod.rs
@@ -1,0 +1,22 @@
+use crate::compiling::v2::Compiler;
+use crate::compiling::CompileResult;
+use rune_ssa::{Block, ValueId};
+
+mod block;
+mod expr;
+mod item_fn;
+mod prelude;
+
+/// Compiler trait implemented for things that can be compiled.
+///
+/// This is the new compiler trait to implement.
+pub(crate) trait Assemble {
+    /// Walk the current type with the given item.
+    fn assemble(&self, block: &Block, c: &mut Compiler<'_>) -> CompileResult<ValueId>;
+}
+
+/// Assemble a function.
+pub(crate) trait AssembleFn {
+    /// Assemble a function.
+    fn assemble_fn(&self, c: &mut Compiler<'_>, instance_fn: bool) -> CompileResult<Block>;
+}

crates/rune/src/compiling/v2/assemble/prelude.rs
@@ -1,0 +1,5 @@
+pub(crate) use crate::ast;
+pub(crate) use crate::compiling::v2::{Assemble, AssembleFn, Compiler};
+pub(crate) use crate::parsing::ParseErrorKind;
+pub(crate) use crate::{CompileError, CompileErrorKind, CompileResult, Spanned};
+pub(crate) use rune_ssa::{Block, Constant, ValueId};

crates/rune/src/compiling/v2/mod.rs
@@ -1,0 +1,53 @@
+use crate::compiling::CompileVisitor;
+use crate::parsing::{Resolve, ResolveError};
+use crate::query::Query;
+use crate::shared::Consts;
+use crate::{Diagnostics, Options, Storage, UnitBuilder};
+use runestick::{Context, Location, Source, Span};
+use std::rc::Rc;
+use std::sync::Arc;
+
+pub(crate) mod assemble;
+pub(crate) mod scope;
+
+pub(crate) use self::assemble::{Assemble, AssembleFn};
+
+#[allow(unused)]
+pub(crate) struct Compiler<'a> {
+    /// Program being compiled.
+    pub(crate) sm: &'a mut rune_ssa::Program,
+    /// The source id of the source.
+    pub(crate) location: Location,
+    /// The source we are compiling for.
+    pub(crate) source: &'a Arc<Source>,
+    /// The current scope stack.
+    pub(crate) scope: scope::Stack,
+    /// The current macro context.
+    pub(crate) storage: &'a Storage,
+    /// The context we are compiling for.
+    pub(crate) context: &'a Context,
+    /// Constants storage.
+    pub(crate) consts: &'a Consts,
+    /// Query system to compile required items.
+    pub(crate) query: &'a Query,
+    /// The compilation unit we are compiling for.
+    pub(crate) unit: UnitBuilder,
+    /// Context for which to emit warnings.
+    pub(crate) contexts: Vec<Span>,
+    /// Enabled optimizations.
+    pub(crate) options: &'a Options,
+    /// Compilation diagnostics.
+    pub(crate) diagnostics: &'a mut Diagnostics,
+    /// Compiler visitor.
+    pub(crate) visitor: Rc<dyn CompileVisitor>,
+}
+
+impl<'a> Compiler<'a> {
+    /// Resolve the given value.
+    pub(crate) fn resolve<T>(&self, value: &T) -> Result<T::Output, ResolveError>
+    where
+        T: Resolve<'a>,
+    {
+        value.resolve(&*self.storage, self.source)
+    }
+}